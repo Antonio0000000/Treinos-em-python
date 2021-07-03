84023b1d86ce336821553d562769398b814ba434
Antonio0000000 <lima_rafael3399@hotmail.com>
2021-07-03 13:28:53 -0300

    Defs criados
    Esse código tá uma loucura kkkkkkk

--- JOKENPO.py
+++ JOKENPO.py
@@ -1,22 +1,16 @@
 # Crie um programa para o pc jogar Jokenpô com você.
 
 from time import sleep
-from random import choice
-from os import system, name
 
-red = '\033[31m'
-green = '\033[32m'
-blue = '\033[34m'
-noColor = '\033[m'
 
-losses = 0
-wins = 0
-empate = 0
-ans = 'S'
-
-print('=' * 9, f'{"JOKENPÔ":^6}', ('=' * 9))
-
-while True:
+def escolha():
+    """
+    -> Deve-se usar as variáveis 'pc' e 'user' no codigo global
+    :return: pc, user
+    """
+    global pc
+    global user
+    from random import choice
     pc = ('tesoura', 'papel', 'pedra')
     pc = choice(pc)
     print('PEDRA *** PAPEL *** TESOURA ')
@@ -25,51 +19,95 @@ while True:
         user = input(f'{red}Opção inválida!{noColor}'
                      f'\nFaça sua escolha novamente: ').strip().lower()
 
+
+def verifywin(item1, item2):
+    """
+    -> Verifica quem venceu.
+    :param item1: máquina
+    :param item2: jogador
+    :return: 'Loss' se item1 vencer, 'Win' se item1 perder ou 'Draw' se empatar
+    """
+
+    if item1 == 'tesoura' and item2 == 'papel':
+        return 'Loss'
+    elif item1 == 'papel' and item2 == 'pedra':
+        return 'Loss'
+    elif item1 == 'pedra' and item2 == 'tesoura':
+        return 'Loss'
+    elif item2 == 'tesoura' and item1 == 'papel':
+        return 'Win'
+    elif item2 == 'papel' and item1 == 'pedra':
+        return 'Win'
+    elif item2 == 'pedra' and item1 == 'tesoura':
+        return 'Win'
+    else:
+        return 'Draw'
+
+
+def continuar():
+    from os import system, name
+    resposta = input('\nDeseja jogar novamente? <S>/<N> ').strip().upper()
+
+    while resposta != 'N' and resposta != 'S':
+        print(f'{red}Opção inválida!{noColor}')
+        resposta = input('\nDeseja jogar novamente? <S>/<N> ').strip().upper()
+    if resposta == 'N':
+        system('cls' if name == 'nt' else 'clear')
+        return False
+    elif resposta == 'S':
+        system('cls' if name == 'nt' else 'clear')
+        return True
+
+
+def encerramento(wins, losses, draw):
+    print('=' * 9, f'{"Jogo encerrado":^6}', ('=' * 9))
+    print(f'{green}Vitórias: \t{wins}'
+          f'\n{red}Derrotas: \t{losses}'
+          f'\n{blue}Empates: \t{draw}{noColor}')
+    print('=' * 9, f'{"Volte sempre!":^6}', ('=' * 9))
+
+
+red = '\033[31m'
+green = '\033[32m'
+blue = '\033[34m'
+yellow = '\033[33m'
+noColor = '\033[m'
+
+perdas = 0
+vitorias = 0
+empates = 0
+pc = ''
+user = ''
+
+print('=' * 9, f'{"JOKENPÔ":^6}', ('=' * 9))
+
+while True:
+    escolha()
+
     print('JO...')
     sleep(1)
     print('KEN...')
     sleep(1)
     print('PÔ!')
-    print(f'\nO computador escolheu {pc}'
-          f'\nE você escolheu {user}')
+    sleep(0.5)
+    print(f'\nVocê escolheu {user.upper()}'
+          f'\nO computador escolheu {pc.upper()}')
 
-    if pc == 'tesoura' and user == 'papel':
-        print(f'{red}Você perdeu...{noColor}')
-        losses += 1
-    elif pc == 'papel' and user == 'pedra':
-        print(f'{red}Você perdeu...{noColor}')
-        losses += 1
-    elif pc == 'pedra' and user == 'tesoura':
-        print(f'{red}Você perdeu...{noColor}')
-        losses += 1
-
-    elif user == 'tesoura' and pc == 'papel':
+    resultado = verifywin(pc, user)
+    if resultado == "Win":
         print(f'{green}Você venceu!{noColor}')
-        wins += 1
-    elif user == 'papel' and pc == 'pedra':
-        print(f'{green}Você venceu!{noColor}')
-        wins += 1
-    elif user == 'pedra' and pc == 'tesoura':
-        print(f'{green}Você venceu!{noColor}')
-        wins += 1
-    else:
+        vitorias += 1
+    elif resultado == 'Loss':
+        print(f'{red}Você perdeu...{noColor}')
+        perdas += 1
+    elif resultado == 'Draw':
         print(f'{blue}Empatou{noColor}')
-        empate += 1
+        empates += 1
 
-    ans = input('\nDeseja jogar novamente? <S>/<N> ').strip().upper()
-
-    while ans != 'N' and ans != 'S':
-        print(f'{red}Opção inválida!{noColor}')
-        ans = input('\nDeseja jogar novamente? <S>/<N> ').strip().upper()
-    if ans == 'N':
-        system('cls' if name == 'nt' else 'clear')
+    ans = continuar()
+    if not ans:
         break
-    elif ans == 'S':
-        system('cls' if name == 'nt' else 'clear')
+    else:
         continue
 
-print('=' * 9, f'{"Jogo encerrado":^6}', ('=' * 9))
-print(f'{green}Vitórias: \t{wins}'
-      f'\n{red}Derrotas: \t{losses}'
-      f'\n{blue}Empates: \t{empate}{noColor}')
-print('=' * 9, f'{"Volte sempre!":^6}', ('=' * 9))
+encerramento(vitorias, perdas, empates)
